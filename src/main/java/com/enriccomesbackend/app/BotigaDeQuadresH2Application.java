5138c897d2528763c2ac6263d5567a68373a0f66
Enric <enric.comes@gmail.com>
2020-10-07 21:49:46 +0200

    First Commit

---
+++ src/main/java/com/enriccomesbackend/app/BotigaDeQuadresH2Application.java
@@ -0,0 +1,13 @@
+package com.enriccomesbackend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotigaDeQuadresH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotigaDeQuadresH2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/enriccomesbackend/app/service/PictureService.java
@@ -0,0 +1,16 @@
+package com.enriccomesbackend.app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.enriccomesbackend.app.entity.Picture;
+
+public interface PictureService {
+
+	public List<Picture> findAllPicturesByShop(Long shopId);
+		
+	public Picture createPicture(Picture picture, Long shopId);
+	
+	public void deleteAllPicturesByShop(Long shopId);	
+	
+}
